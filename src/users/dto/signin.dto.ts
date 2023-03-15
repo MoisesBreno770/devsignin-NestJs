ab60f7ff73380127564cdc35a1e2c943ffa09e5e
Moises Breno <moisesbreno7@gmail.com>
2023-03-15 03:38:00 -0300

    first commit

---
+++ src/users/dto/signin.dto.ts
@@ -0,0 +1,14 @@
+import { IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator/types/decorator/decorators";
+
+export class SigninDto {
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    password: string;
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose'
+import { UsersSchema } from './schemas/users.schemas';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+
+      }
+    ]),
+    AuthModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
